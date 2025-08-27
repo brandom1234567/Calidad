9dcbe315c8e20010409868fe419ee74ae52c0d81
brandom123456 <brandonmella0@gmail.com>
2025-08-27 18:47:33 -0400

    commit3

--- test_sample.py
+++ test_sample.py
@@ -3,3 +3,9 @@ from app.calculate import hipotenusa
 def test_vista():
     assert hipotenusa(3,4) == 5
 
+
+from app.calculate import hipotenusa
+
+def test_vista():
+    assert hipotenusa(4,6) == 6
+
