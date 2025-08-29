92e9af7722f882145e76157b5611c4b4b43ae2de
brandom1234567 <brandonmella0@gmail.com>
2025-08-29 18:02:43 -0400

    segundo testeo

--- test_sample.py
+++ test_sample.py
@@ -3,9 +3,6 @@ from app.calculate import hipotenusa
 def test_vista():
     assert hipotenusa(3,4) == 5
 
-
-from app.calculate import hipotenusa
-
-def test_vista():
-    assert hipotenusa(4,6) == 6
+def test_otro_valor():
+    assert hipotenusa(5, 12) == 13
 
